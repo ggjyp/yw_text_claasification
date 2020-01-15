23929582fd7be6568e4bcc230ab34a3f6ccad101
jiangyipeng <jypsdly@163.cpm>
2020-01-15 18:28:52 +0800

    添加后端接口

--- demo.py
+++ demo.py
@@ -1,20 +1,38 @@
+# -*- coding: utf-8 -*-
+import sys
 import fasttext
-import jieba
 from utils import preprocess
+import jieba
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
-model = fasttext.load_model('model/1579082291.model')
 
-text = ' 澳洲毕业证【Q/微457202606】新西兰怀卡托大学学位证成绩单/办澳洲新西兰怀卡托大学毕业证成绩单/官方学历认证本科/硕士 (The University of Waikato'
-content = preprocess.get_processed_text([text])
-content = jieba.cut(content)
-content = " ".join(content)
-print("content:", content)
-result = model.predict(content)
-label = result[0][0]
-score = result[1]
-if label == '__label__1':
-    label = '广告书'
-else:
-    label = '非广告书'
-res = {"label": label, "score": score[0]}
-print("result:", res)
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/text_classify')
+def hello_world():
+    text = request.args.get("text")
+    content = preprocess.get_processed_text([text])
+    content = jieba.cut(content)
+    content = " ".join(content)
+    print("content:", content)
+    result = model.predict(content)
+    label = result[0][0]
+    score = result[1]
+    if label == '__label__1':
+        label = '广告书'
+    else:
+        label = '非广告书'
+    res = {"label": label, "score": score[0]}
+    print("result:", res)
+    return jsonify(res)
+
+
+if __name__ == "__main__":
+    model_path = 'model/my_model.bin'
+    if len(sys.argv) == 2:
+        model_path = 'model/' + sys.argv[1]
+    model = fasttext.load_model(model_path)
+    app.run(host='0.0.0.0', port=13000)
